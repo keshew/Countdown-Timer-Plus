6f1ff8f53d9067bd2ce61345b9c764c5d7c62a1a
keshew <artyom.korotckov2017@yandex.ru>
2026-01-27 18:57:52 +0500

    Initial Commit

---
+++ Countdown Timer Plus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Countdown Timer Plus
+//
+//  Created by Артём Коротков on 27.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
